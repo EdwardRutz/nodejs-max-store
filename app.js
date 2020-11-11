5cdb2458d1f58baa72a7e7e1f697527d60047c77
Edward Rutz <edward-github@opayq.com>
2020-11-10 19:09:47 -0500

    Added routes

--- app.js
+++ app.js
@@ -3,23 +3,18 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 // Parsing Middleware
 app.use(bodyParser.urlencoded({extended: false}));
 
-// Routing Middleware
-app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page Not Found</h1>');
 });
-
-app.use('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-})
-
-app.use('/', (req, res,next) => {
-  res.send('<h1>Hello from Express!</h1>');
-});
-
 
 
 app.listen(3000);
